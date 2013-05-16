067acd7dcafd0bf0d3e56ac5eb3558178a09457e
oscar <oscar@oscar.(none)>
2013-05-16 15:01:22 +0200

    otsu V1.0

--- process_frame.c
+++ process_frame.c
@@ -12,128 +12,165 @@
 #include <string.h>
 #include <stdlib.h>
 
-OSC_ERR OscVisDrawBoundingBoxBW(struct OSC_PICTURE *picIn, struct OSC_VIS_REGIONS *regions, uint8 Color);
+OSC_ERR OscVisDrawBoundingBoxBW(struct OSC_PICTURE *picIn,
+		struct OSC_VIS_REGIONS *regions, uint8 Color);
 
-void ProcessFrame(uint8 *pInputImg)
-{
+void ProcessFrame(uint8 *pInputImg) {
+
 	int c, r;
-	int nc = OSC_CAM_MAX_IMAGE_WIDTH/2;
+	int nc = OSC_CAM_MAX_IMAGE_WIDTH / 2;
 	int siz = sizeof(data.u8TempImage[GRAYSCALE]);
 
-	int Shift = 7;
-	short Beta = 2;//the meaning is that in floating point the value of Beta is = 6/(1 << Shift) = 6/128 = 0.0469
-	uint8 MaxForeground = 120;//the maximum foreground counter value (at 15 fps this corresponds to less than 10s)
+	//int Shift = 7;
+	//short Beta = 2;//the meaning is that in floating point the value of Beta is = 6/(1 << Shift) = 6/128 = 0.0469
+	//uint8 MaxForeground = 120;//the maximum foreground counter value (at 15 fps this corresponds to less than 10s)
 
 	struct OSC_PICTURE Pic1, Pic2;//we require these structures to use Oscar functions
 	struct OSC_VIS_REGIONS ImgRegions;//these contain the foreground objects
 
-	if(data.ipc.state.nStepCounter == 1)
-	{
+	/* ********************************************** AUfgabe 2 ************************************************** */
+	// Variablen für Testat2
 
-	}
-	else
+	uint32 Hist[256];
+	uint32 omega0, omega1, u0, u1;
+	float sigma_b, sigma_b_max;
+	short index;
+	int y, k;
+
+	memset(Hist, 0, sizeof(Hist));
+
+	for (r = 0; r < siz; r += nc)/* we strongly rely on the fact that them images have the same size */
 	{
+		for (c = 0; c < nc; c++) {
+			/* first determine the foreground estimate */
+			Hist[data.u8TempImage[GRAYSCALE][r + c]]++;
+		}
+	}
+
+	sigma_b_max = 0;
+
+	for (k = 0; k < 255; k++) {
+
+		omega0 = 0;
+		omega1 = 0;
+		u0 = 0;
+		u1 = 0;
+
+		for (y = 0; y < 255; y++) {
+			if (y <= k) {
+				omega0 += Hist[y];
+				u0 += Hist[y] * y;
+			} else {
+				omega1 += Hist[y];
+				u1 += Hist[y] * y;
+			}
+		}
+		u0 = u0 / omega0;
+		u1 = u1 / omega1;
+
+		sigma_b = omega0 * omega1 * (u0 - u1) * (u0 - u1);
+
+		if (sigma_b_max < sigma_b) {
+			sigma_b_max = sigma_b;
+			index = k;
+		}
+	}
+
+	if (data.ipc.state.nStepCounter == 1) {
+		/* this is the first time we call this function */
+		/* first time we call this; index 1 always has the background image */
+		memcpy(data.u8TempImage[BACKGROUND], data.u8TempImage[GRAYSCALE],
+				sizeof(data.u8TempImage[GRAYSCALE]));
+		/* set foreground counter to zero */
+		memset(data.u8TempImage[FGRCOUNTER], 0,
+				sizeof(data.u8TempImage[FGRCOUNTER]));
+	} else {
 
 		/* ********************************************** AUfgabe 1 ************************************************** */
 
 		/* this is the default case */
-		for(r = 0; r < siz; r+= nc)/* we strongly rely on the fact that them images have the same size */
+		for (r = 0; r < siz; r += nc)/* we strongly rely on the fact that them images have the same size */
 		{
-			for(c = 0; c < nc; c++)
-			{
+			for (c = 0; c < nc; c++) {
 				/* first determine the foreground estimate */
-				data.u8TempImage[THRESHOLD][r+c] = data.u8TempImage[GRAYSCALE][r+c] > data.ipc.state.nThreshold ? 0 : 0xff;
-
+				data.u8TempImage[THRESHOLD][r + c]
+						= data.u8TempImage[GRAYSCALE][r + c] > index ? 0 : 0xff;
 			}
 		}
 
-		/* ********************************************** Histogramm ************************************************** */
-		int Hist[256];
 
-		for(r = 0; r < siz; r+= nc)/* we strongly rely on the fact that them images have the same size */
+		/* ********************************************** Uebung 12 ************************************************** */
+
+		for (r = nc; r < siz - nc; r += nc)/* we skip the first and last line */
 		{
-			for(c = 0; c < nc; c++)
+			for (c = 1; c < nc - 1; c++)/* we skip the first and last column */
 			{
-				/* first determine the foreground estimate */
-				c = data.u8TempImage[GRAYSCALE][r+c];
-				Hist[c]++;
+				unsigned char* p = &data.u8TempImage[THRESHOLD][r + c];
+				data.u8TempImage[EROSION][r + c] = *(p - nc - 1) & *(p - nc)
+						& *(p - nc + 1) & *(p - 1) & *p & *(p + 1) & *(p + nc
+						- 1) & *(p + nc) & *(p + nc + 1);
 			}
 		}
-		/* *********************************************************************************************************** */
 
-		for(r = nc; r < siz-nc; r+= nc)/* we skip the first and last line */
+		for (r = nc; r < siz - nc; r += nc)/* we skip the first and last line */
 		{
-			for(c = 1; c < nc-1; c++)/* we skip the first and last column */
+			for (c = 1; c < nc - 1; c++)/* we skip the first and last column */
 			{
-				unsigned char* p = &data.u8TempImage[EROSION][r+c];
-				data.u8TempImage[DILATION][r+c] = *(p-nc-1) | *(p-nc) | *(p-nc+1) |
-												  *(p-1)    | *p      | *(p+1)    |
-												  *(p+nc-1) | *(p+nc) | *(p+nc+1);
+				unsigned char* p = &data.u8TempImage[EROSION][r + c];
+				data.u8TempImage[DILATION][r + c] = *(p - nc - 1) | *(p - nc)
+						| *(p - nc + 1) | *(p - 1) | *p | *(p + 1) | *(p + nc
+						- 1) | *(p + nc) | *(p + nc + 1);
 			}
 		}
-
-		for(r = nc; r < siz-nc; r+= nc)/* we skip the first and last line */
-		{
-			for(c = 1; c < nc-1; c++)/* we skip the first and last column */
-			{
-				unsigned char* p = &data.u8TempImage[THRESHOLD][r+c];
-				data.u8TempImage[EROSION][r+c] = *(p-nc-1) & *(p-nc) & *(p-nc+1) &
-												 *(p-1)    & *p      & *(p+1)    &
-												 *(p+nc-1) & *(p+nc) & *(p+nc+1);
-			}
-		}
-
 
 		//wrap image DILATION in picture struct
 		Pic1.data = data.u8TempImage[DILATION];
 		Pic1.width = nc;
-		Pic1.height = OSC_CAM_MAX_IMAGE_HEIGHT/2;
+		Pic1.height = OSC_CAM_MAX_IMAGE_HEIGHT / 2;
 		Pic1.type = OSC_PICTURE_GREYSCALE;
 		//as well as EROSION (will be used as output)
 		Pic2.data = data.u8TempImage[EROSION];
 		Pic2.width = nc;
-		Pic2.height = OSC_CAM_MAX_IMAGE_HEIGHT/2;
+		Pic2.height = OSC_CAM_MAX_IMAGE_HEIGHT / 2;
 		Pic2.type = OSC_PICTURE_BINARY;//probably has no consequences
 		//have to convert to OSC_PICTURE_BINARY which has values 0x01 (and not 0xff)
 		OscVisGrey2BW(&Pic1, &Pic2, 0x80, false);
 
 		//now do region labeling and feature extraction
-		OscVisLabelBinary( &Pic2, &ImgRegions);
-		OscVisGetRegionProperties( &ImgRegions);
+		OscVisLabelBinary(&Pic2, &ImgRegions);
+		OscVisGetRegionProperties(&ImgRegions);
 
 		//OscLog(INFO, "number of objects %d\n", ImgRegions.noOfObjects);
 		//plot bounding boxes both in gray and dilation image
 		Pic2.data = data.u8TempImage[GRAYSCALE];
-		OscVisDrawBoundingBoxBW( &Pic2, &ImgRegions, 255);
-		OscVisDrawBoundingBoxBW( &Pic1, &ImgRegions, 128);
+		OscVisDrawBoundingBoxBW(&Pic2, &ImgRegions, 255);
+		OscVisDrawBoundingBoxBW(&Pic1, &ImgRegions, 128);
 	}
 }
 
-
-/* Drawing Function for Bounding Boxes; own implementation because Oscar only allows colored boxes; here in Gray value "Color"  */
+/* Drawing Function for Bounding Boxes; own implementation because Oscar only allows colored boxes; here in Gray value "Color" */
 /* should only be used for debugging purposes because we should not drawn into a gray scale image */
-OSC_ERR OscVisDrawBoundingBoxBW(struct OSC_PICTURE *picIn, struct OSC_VIS_REGIONS *regions, uint8 Color)
-{
-	 uint16 i, o;
-	 uint8 *pImg = (uint8*)picIn->data;
-	 const uint16 width = picIn->width;
-	 for(o = 0; o < regions->noOfObjects; o++)//loop over regions
-	 {
-		 /* Draw the horizontal lines. */
-		 for (i = regions->objects[o].bboxLeft; i < regions->objects[o].bboxRight; i += 1)
-		 {
-				 pImg[width * regions->objects[o].bboxTop + i] = Color;
-				 pImg[width * (regions->objects[o].bboxBottom - 1) + i] = Color;
-		 }
+OSC_ERR OscVisDrawBoundingBoxBW(struct OSC_PICTURE *picIn,
+		struct OSC_VIS_REGIONS *regions, uint8 Color) {
+	uint16 i, o;
+	uint8 *pImg = (uint8*) picIn->data;
+	const uint16 width = picIn->width;
+	for (o = 0; o < regions->noOfObjects; o++)//loop over regions
+	{
+		/* Draw the horizontal lines. */
+		for (i = regions->objects[o].bboxLeft; i
+				< regions->objects[o].bboxRight; i += 1) {
+			pImg[width * regions->objects[o].bboxTop + i] = Color;
+			pImg[width * (regions->objects[o].bboxBottom - 1) + i] = Color;
+		}
 
-		 /* Draw the vertical lines. */
-		 for (i = regions->objects[o].bboxTop; i < regions->objects[o].bboxBottom-1; i += 1)
-		 {
-				 pImg[width * i + regions->objects[o].bboxLeft] = Color;
-				 pImg[width * i + regions->objects[o].bboxRight] = Color;
-		 }
-	 }
-	 return SUCCESS;
+		/* Draw the vertical lines. */
+		for (i = regions->objects[o].bboxTop; i
+				< regions->objects[o].bboxBottom - 1; i += 1) {
+			pImg[width * i + regions->objects[o].bboxLeft] = Color;
+			pImg[width * i + regions->objects[o].bboxRight] = Color;
+		}
+	}
+	return SUCCESS;
 }
-
 
